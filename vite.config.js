371a1a34b821bbf6b5ec3fc106b6c60c20f61a52
Michael Collins <michael-collins@users.noreply.github.com>
2025-01-11 10:18:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/word-tumbler-googlesheets/',
+  base: '/data-tumbler-googlesheets/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
